e0f4ecd6409be46b2cbc7b4a366ebe8ab57ce9ad
Rahul Thakoor <rahul@balena.io>
2021-04-16 10:51:13 +0400

    Fix audio rendering error when using alsa bridge
    Change-type: patch
    Signed-off-by: Rahul Thakoor <rahul@balena.io>

--- build-images.sh
+++ build-images.sh
@@ -15,7 +15,7 @@ function build_and_push_image () {
   echo "Building for machine name $BALENA_MACHINE_NAME, platform $DOCKER_ARCH using dockerfile from $DOCKERFILE_DIR, pushing to $DOCKER_REPO/browser"
 
   sed "s/%%BALENA_MACHINE_NAME%%/$BALENA_MACHINE_NAME/g" $DOCKERFILE_DIR/Dockerfile.template > ./Dockerfile.$BALENA_MACHINE_NAME
-  docker buildx build -t $DOCKER_REPO/browser:$BALENA_MACHINE_NAME --platform $DOCKER_ARCH --file Dockerfile.$BALENA_MACHINE_NAME .
+  docker buildx build -t $DOCKER_REPO/browser:V1-$BALENA_MACHINE_NAME --platform $DOCKER_ARCH --file Dockerfile.$BALENA_MACHINE_NAME .
 
   echo "Publishing..."
   docker push $DOCKER_REPO/browser:$BALENA_MACHINE_NAME
@@ -26,8 +26,8 @@ function build_and_push_image () {
 
 function retag_and_push_image () {
   local DOCKER_REPO=$1
-  local BUILT_TAG=$2
-  local NEW_TAG=$3
+  local BUILT_TAG="V1-$2"
+  local NEW_TAG="V1-$3"
 
   echo "Taging $DOCKER_REPO/browser:$BUILT_TAG as $DOCKER_REPO/browser:$NEW_TAG"
   docker tag $DOCKER_REPO/browser:$BUILT_TAG $DOCKER_REPO/browser:$NEW_TAG
@@ -38,13 +38,19 @@ function retag_and_push_image () {
 
 # YOu can pass in a repo (such as a test docker repo) or accept the default
 DOCKER_REPO=${1:-balenablocks}
+BLOCK_NAME="browser"
 
 #RPI4 is built as ARMv7 because the base Raspian image is 32-bit
 build_and_push_image $DOCKER_REPO "raspberrypi3" "linux/arm/v7"
+build_and_push_image $DOCKER_REPO "genericx86-64-ext" "linux/amd64"
 
-#only need to build once per arch, and retag & push for clones
+# #only need to build once per arch, and retag & push for clones
 retag_and_push_image $DOCKER_REPO "raspberrypi3" "raspberrypi4-64"
-retag_and_push_image $DOCKER_REPO "raspberrypi3" "raspberrypi3-64"
+# # retag_and_push_image $DOCKER_REPO "raspberrypi3" "raspberrypi3-64"
 retag_and_push_image $DOCKER_REPO "raspberrypi3" "fincm3"
 
-build_and_push_image $DOCKER_REPO "genericx86-64-ext" "linux/amd64"
+# docker manifest rm $DOCKER_REPO/$BLOCK_NAME:V1 || true
+docker manifest create $DOCKER_REPO/$BLOCK_NAME:V1 --amend $DOCKER_REPO/$BLOCK_NAME:V1-raspberrypi3 --amend $DOCKER_REPO/$BLOCK_NAME:V1-raspberrypi4-64 --amend $DOCKER_REPO/$BLOCK_NAME:V1-genericx86-64-ext
+docker manifest annotate --arch arm64 $DOCKER_REPO/$BLOCK_NAME:V1 $DOCKER_REPO/$BLOCK_NAME:V1-raspberrypi4-64
+docker manifest annotate --variant v8 $DOCKER_REPO/$BLOCK_NAME:V1 $DOCKER_REPO/$BLOCK_NAME:V1-raspberrypi4-64
+docker manifest push $DOCKER_REPO/$BLOCK_NAME:V1
--- start.sh
+++ start.sh
@@ -80,8 +80,8 @@ echo "#!/bin/bash" > /home/chromium/xstart.sh
 
 # Configure audio block server
 # This won't have any effect if not using the audio block
-PULSE_SERVER=${PULSE_SERVER:-tcp:audio:4317}
-echo "export PULSE_SERVER=$PULSE_SERVER" >> /home/chromium/xstart.sh
+# PULSE_SERVER=${PULSE_SERVER:-tcp:audio:4317}
+# echo "export PULSE_SERVER=$PULSE_SERVER" >> /home/chromium/xstart.sh
 
 # if no window size has been specified, find the framebuffer size and use that
 if [[ -z ${WINDOW_SIZE+x} ]]
